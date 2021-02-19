86dab96840d8586956c39354e2cf9446077e2ac5
Dilfina <190103057@stu.sdu.edu.kz>
2021-02-20 00:25:02 +0600

    adding a route to skills page

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Route::get('/skills', function () {
+    return view('skills');
+})->name('skills');
