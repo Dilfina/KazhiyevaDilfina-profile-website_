2eab8b9998b50d5aa24020a3bc6f427a804172d1
Dilfina <190103057@stu.sdu.edu.kz>
2021-03-16 12:34:40 +0600

    Lab7 is completed where I have created an API to return all posts in our database and another one which returns the row ith specified post_id

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Posts;
+
+class ApiController extends Controller
+{
+	/*
+	This function returns all posts rows
+	*/
+    public function index(Request $request){
+    	$posts = Posts::all();
+    	return response($posts, 200);
+
+    }
+    /*
+    Returns the post with specified id
+
+    */
+
+    public function get_post(Request $request){
+    	$post=Posts::find($request->post_id);
+
+    	if($post==null){
+    		return response(['message'=>'There is no post with that id'], 404);
+    	}
+    	return response($post, 200);
+
+
+    }
+}
